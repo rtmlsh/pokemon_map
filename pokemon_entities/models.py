99c23acf8770fa89de9dffd66a6f898d6e105cd2
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2022-03-07 20:02:50 +0300

    Add model location fields Pokemon

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,8 +4,12 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, blank=True)
-    picture = models.ImageField(upload_to='pokemons', null=True)
+    picture = models.ImageField(upload_to='pokemons_images', null=True)
 
     def __str__(self):
         return f'{self.title}'
 
+class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
