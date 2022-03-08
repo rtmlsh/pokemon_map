6c6181f826378d3c0edda26c586f05b560c8f090
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2022-03-08 19:51:19 +0300

    Show pokemon on exact page

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -55,24 +55,23 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
-
-    for pokemon in pokemons:
-        if pokemon['pokemon_id'] == int(pokemon_id):
-            requested_pokemon = pokemon
-            break
-    else:
-        return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
-
+    pokemon = Pokemon.objects.filter(id=pokemon_id)[0]
+    pokemon_entities = PokemonEntity.objects.filter(pokemon=pokemon)
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon_entity in requested_pokemon['entities']:
+
+    for spec in pokemon_entities:
         add_pokemon(
-            folium_map, pokemon_entity['lat'],
-            pokemon_entity['lon'],
-            pokemon['img_url']
+            folium_map, spec.latitude,
+            spec.longitude,
+            request.build_absolute_uri(pokemon.picture.url)
         )
 
+    pokemon_on_page = {
+        'pokemon_id': pokemon.id,
+        'img_url': request.build_absolute_uri(pokemon.picture.url),
+        'title_ru': pokemon.title,
+    }
+
     return render(request, 'pokemon.html', context={
-        'map': folium_map._repr_html_(), 'pokemon': pokemon
+        'map': folium_map._repr_html_(), 'pokemon': pokemon_on_page
     })
